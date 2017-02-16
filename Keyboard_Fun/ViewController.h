07a953a59f4480cc7842e2d3c8b90bc421635b1a
Chloe Marshall <chloe.r.marshall@gmail.com>
2017-02-16 12:40:11 -0600

    Committing Keyboard project done for iPhone development class

--- Keyboard_Fun/ViewController.h
+++ Keyboard_Fun/ViewController.h
@@ -8,7 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    UITextField *activeField;
+    //Placeholder for the active textbox
+}
+@property (weak, nonatomic) IBOutlet UITextField *textbox1;
+@property (weak, nonatomic) IBOutlet UITextField *textbox2;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+- (IBAction)doneWithKeyboard:(id)sender;
+- (IBAction)didBeginEditing:(id)sender;
+- (IBAction)didEndEditing:(id)sender;
+- (void) keyboardWasShown: (NSNotification *) aNotification;
+- (void) keyboardWillBeHidden: (NSNotification *) aNotification;
 
 
 @end
--- Keyboard_Fun/ViewController.m
+++ Keyboard_Fun/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 
@@ -26,4 +29,42 @@
 }
 
 
+- (IBAction)doneWithKeyboard:(id)sender {
+    activeField = nil;
+    [sender resignFirstResponder];
+}
+
+- (IBAction)didBeginEditing:(id)sender {
+    //how come we didnt have to sythesize this variable;
+    activeField = sender;
+}
+
+- (IBAction)didEndEditing:(id)sender {
+    activeField = nil;
+}
+
+- (void) keyboardWasShown: (NSNotification *) aNotification{
+    NSDictionary *info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    
+    _scrollView.contentInset = contentInsets;
+    _scrollView.scrollIndicatorInsets = contentInsets;
+    
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    
+    if(!CGRectContainsPoint(aRect, activeField.frame.origin)){
+        CGPoint scrollPoint = CGPointMake(0.0, activeField.frame.origin.y-kbSize.height);
+        [_scrollView setContentOffset: scrollPoint animated:YES];
+        
+    }
+    
+}
+- (void) keyboardWillBeHidden: (NSNotification *) aNotification{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    _scrollView.contentInset = contentInsets;
+    _scrollView.scrollIndicatorInsets = contentInsets;
+    
+}
 @end
